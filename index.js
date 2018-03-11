ff6ec42712b523e9027cf22b29ddcf8cbaa4829e
simalexan <alexander.simovic@gmail.com>
2018-03-11 20:11:26 +0100

    updated code from the stream on 11.03

--- index.js
+++ index.js
@@ -1,57 +1,59 @@
-const botBuilder = require('claudia-bot-builder');
+const alexaSkillKit = require('alexa-skill-kit');
 const AlexaMessageBuilder = require('alexa-message-builder');
 const rp = require('minimal-request-promise');
-const CLIENT_ID = '<YOUR-TWITCH-CLIENT-ID>';
+const CLIENT_ID = process.env.TWITCH_CLIENT_ID;
 
-module.exports = botBuilder(function (message){
-  let intent = message.originalRequest.request.intent;
-  if (!intent) {
+exports.handler = function (event, context) {
+  return alexaSkillKit(event, context, message => {
+
+    let intent = message.intent;
+
+
+
+    if (intent.name == 'MostWatchedGameStreamer'){
+      let gameName = intent.slots.game.value.toLowerCase();
+      let platform = intent.slots.platform.value.toLowerCase();
+
+      console.log(gameName);
+      console.log(platform);
+
+      if (platform != 'twitch') {
+        return new AlexaMessageBuilder().addText(`Sorry, ${platform} is not supported yet! Please try at a later time!`).get();
+      }
+
+      return rp.get(`https://api.twitch.tv/helix/games?name=${gameName}`,{
+        headers: { 'Client-ID': CLIENT_ID }
+      }).then(gameResponse => {
+        let games = JSON.parse(gameResponse.body).data;
+        if (!games || games.length < 1){
+            return new AlexaMessageBuilder().addText('There isn\'t such a game, sorry! Try again.').get();
+        }
+        let selectedGameId = games[0].id;
+        console.log(selectedGameId);
+        return rp.get(`https://api.twitch.tv/helix/streams?game_id=${selectedGameId}`,
+          { headers: { 'Client-ID': CLIENT_ID } });
+      }).then(streamsResponse => {
+        let streams = JSON.parse(streamsResponse.body).data;
+        if (!streams || streams.length < 1) {
+          return new AlexaMessageBuilder().addText('No streams for this game, sorry!').get();
+        }
+        let mostWatchedStream = streams[0];
+        console.log(mostWatchedStream);
+        return new AlexaMessageBuilder()
+          .addText(`The most watched stream is ${mostWatchedStream.title}`)
+          .get();
+      })
+    }
+
+    if (intent.name == 'HelloIntent') {
+      return new AlexaMessageBuilder()
+      .addText('Hello there my viewers from Twitch, how are you today?')
+      .get();
+    }
+
     return new AlexaMessageBuilder()
       .addText('Sorry, I didn\'t understand your Streamer question.')
       .get();
-  }
-  if (intent.name == 'MostWatchedGameStreamer'){
-    let gameName = intent.slots.game.value.toLowerCase();
-    let platform = intent.slots.platform.value.toLowerCase();
 
-    console.log(gameName);
-    console.log(platform);
-
-    if (platform != 'twitch') {
-      return new AlexaMessageBuilder().addText(`Sorry, ${platform} is not supported yet! Please try at a later time!`).get();
-    }
-
-	return rp.get(`https://api.twitch.tv/helix/games?name=${gameName}`,{
-      headers: { 'Client-ID': CLIENT_ID }
-    }).then(gameResponse => {
-      let games = JSON.parse(gameResponse.body);
-      if (!games || !games.data || games.data.length < 1){
-          return new AlexaMessageBuilder().addText('There isn\'t such a game, sorry! Try again.').get();
-      }
-      let selectedGameId = games.data[0].id;
-      console.log(selectedGameId);
-      return rp.get(`https://api.twitch.tv/helix/streams?game_id=${selectedGameId}`,
-        { headers: { 'Client-ID': CLIENT_ID } });
-    }).then(streamsResponse => {
-      let streams = JSON.parse(streamsResponse.body);
-      if (!streams || !streams.data || streams.data.length < 1) {
-        return new AlexaMessageBuilder().addText('No streams for this game, sorry').get();
-      }
-      let mostWatchedStream = streams.data[0];
-      console.log(mostWatchedStream);
-      return new AlexaMessageBuilder()
-        .addText(`The most watched stream is ${mostWatchedStream.title}`)
-        .get();
-    })
-  }
-
-  if (intent.name == 'HelloIntent') {
-    return new AlexaMessageBuilder()
-	  .addText('Hello there my viewers from Twitch, how are you today?')
-	  .get();
-  }
-
-  return new AlexaMessageBuilder()
-    .addText('Sorry, I didn\'t quite understand what you wanted, ask me to say hi or to tell you which game\'s streamer is the most watched on some platform.')
-	.get();
-}, { platforms: ['alexa'] });
+  });
+};
